4529d165c1eb2d9bfc8c9fa76904d435dfd3d3f1
Samit Sarkar <samsarka@microsoft.com>
2019-01-22 20:56:36 +0530

    Add project files.

---
+++ RedisDemo/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using StackExchange.Redis;
+
+namespace RedisDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [OutputCache(Duration = 60, VaryByParam = "none")]
+        public ActionResult Outputcache()
+        {
+            ViewBag.Message = "Output cacheing . . . " + DateTime.Now.ToString("T");
+
+            return View();
+        }
+
+        public ActionResult Sessioncache()
+        {
+            Session["demosession"] = "Session state cacheing. . . "  + DateTime.Now.ToString();
+            ViewBag.Message = Session["demosession"];
+            return View();
+        }
+
+        public ActionResult RedisCache()
+        {
+            ViewBag.Message = "A simple example with Azure Redis Cache on ASP.NET.";
+
+            var lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
+                return ConnectionMultiplexer.Connect(cacheConnection);
+            });
+
+            // Connection refers to a property that returns a ConnectionMultiplexer
+            // as shown in the previous example.
+            IDatabase cache = lazyConnection.Value.GetDatabase();
+
+            // Perform cache operations using the cache object...
+
+            // Simple PING command
+            ViewBag.command1 = "PING";
+            ViewBag.command1Result = cache.Execute(ViewBag.command1).ToString();
+
+            // Simple get and put of integral data types into the cache
+            ViewBag.command2 = "GET Message";
+            ViewBag.command2Result = cache.StringGet("Message").ToString();
+
+            ViewBag.command3 = "SET Message \"Hello! The cache is working from ASP.NET!\"";
+            ViewBag.command3Result = cache.StringSet("Message", "Hello! The cache is working from ASP.NET!").ToString();
+
+            // Demostrate "SET Message" executed as expected...
+            ViewBag.command4 = "GET Message";
+            ViewBag.command4Result = cache.StringGet("Message").ToString();
+
+            // Get the client list, useful to see if connection list is growing...
+           // ViewBag.command5 = "CLIENT LIST";
+            //ViewBag.command5Result = cache.Execute("CLIENT", "LIST").ToString().Replace(" id=", "\rid=");
+
+            //lazyConnection.Value.Dispose();
+
+            return View();
+        }
+
+    }
+}
